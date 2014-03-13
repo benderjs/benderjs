b85b22d828d499084add6eca1dccfef0cd098b92
Grzegorz Pabian <greg.pabian@gmail.com>
2014-03-13 12:27:53 +0100

    replaced faye with socket.io

--- static/bender-client.js
+++ static/bender-client.js
@@ -1,34 +1,120 @@
 (function () {
     var host = /^[a-z]+:\/\/([^\/]+)/.exec(window.location)[1].split(":"),
         id = /\/clients\/([^\/]+)/.exec(window.location)[1],
+        statusEl = document.getElementById('status'),
+        contextEl = document.getElementById('context'),
         busy = false,
-        client;
+        socket,
+        states;
 
-    client = new Faye.Client('http://' + host[0] + ':' + (host[1] || 80) + '/faye', {
-        timeout: 120,
-        retry: 1
-    });
-
-    client.subscribe('/execute', function (tests) {
-        if (!busy) runTests(tests);
-    });
-
-    client.disable('autodisconnect');
-    client.addExtension({
-        incoming: function (msg, callback) {
-            if (msg.channel === '/meta/connect' && !msg.successful && (/^401/.test(msg.error))) {
-                window.location = '/capture';
-            }
-            callback(msg);
-        }
-    });
-
-    window.onbeforeunload = function () {
-        console.log(client.publish('/client_disconnect', id));
+    states = {
+        CONNECT: 0,
+        RECONNECT: 1,
+        RECONNECT_FAIL: 2,
+        RECONNECTING: 3,
+        DISCONNECT: 4
     };
 
-    function runTests(tests) {
+    socket = io.connect('http://' + host[0] + ':' + (host[1] || 80));
+    socket.on('connect', setStatus(states.CONNECT));
+    socket.on('reconnect', setStatus(states.RECONNECT));
+    socket.on('reconnect_failed', setStatus(states.RECONNECT_FAIL));
+    socket.on('reconnecting', setStatus(states.RECONNECTING));
+    socket.on('disconnect', setStatus(states.DISCONNECT));
 
+    function setStatus(status) {
+        var messages = ['Connected', 'Reconnected', 'Failed to reconnect',
+            'Reconnecting in ', 'Disconnected'];
+
+        return function (options) {
+            var msg = messages[status];
+            statusEl.innerHTML = msg + (options ? options + 'ms...' : '');
+            statusEl.className = status === states.CONNECT ? 'ok' :
+                (status === states.RECONNECT || status === states.RECONNECTING) ? 'warn' :
+                (status === states.RECONNECT_FAIL || states.DISCONNECT) ? 'fail' : '';
+        };
     }
+
+    function Bender(socket) {
+        this.suite = null;
+        this.results = [];
+
+        this.setup = function (context) {
+            var that = this;
+
+            context.bender = this;
+            context.onerror = this.error;
+
+            function stealLogs(context) {
+                var commands = ['log', 'info', 'warn', 'debug', 'error'],
+                    replace = function(command) {
+                        var old = context.console[command];
+
+                        context.console[command] = function () {
+                            that.log(arguments);
+                            if (old) Function.prototype.apply.call(old, context.console, arguments);
+                        };
+                    },
+                    i;
+
+                context.console = context.console || {};
+
+                for (i = 0; i < commands.length; i++) {
+                    replace(commands[i]);
+                }
+
+                context.alert = function (msg) {
+                    that.log(msg);
+                };
+            }
+            stealLogs();
+        };
+
+        this.error = function (error) {
+            socket.emit('error', error);
+        };
+
+        this.result = function (result) {
+            this.results.push(result);
+            socket.emit.result(result);
+        };
+
+        this.start = function () {
+            // TODO start testing here
+        };
+
+        this.next = function () {
+            var test = this.suite.unshift();
+            contextEl.src = test.src;
+        };
+
+        this.complete = function (result) {
+            socket.emit('complete', result);
+            contextEl.src = 'about:blank';
+        };
+
+        this.log = function (args) {
+            // TODO add some formatting, maybe stringify objects?
+            socket.emit('log', Array.prototype.join.call(arguments, ' '));
+        };
+
+
+        function register() {
+            socket.emit('register', {
+                id: id,
+                userAgent: navigator.userAgent
+            });
+        }
+
+        function runTests(suite) {
+            this.suite = suite;
+            this.next();
+        }
+
+        socket.on('connect', register);
+        socket.on('run', runTests);
+    }
+
+    window.bender = new Bender(socket);
 
 })();
--- static/main.js
+++ static/main.js
@@ -1,12 +1,5 @@
 (function () {
-    var client = new Faye.Client('/faye', {
-            timeout: 120
-        }),
-        clientsEl = document.getElementById('clients');
-
-    client.subscribe('/client_list', function (list) {
-        updateClientList(list);
-    });
+        
 
     function updateClientList(list) {
         clients.innerHTML = list.map(function (client) {
