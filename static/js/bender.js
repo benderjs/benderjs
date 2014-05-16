72e3baafcf9aed38c7398e37f6f90eaa46842c08
Grzegorz Pabian <greg.pabian@gmail.com>
2014-05-16 10:21:41 +0200

    removed obsolete code from dashboard runner script

--- static/js/bender.js
+++ static/js/bender.js
@@ -73,35 +73,9 @@
             this.complete();
         };
 
-        this.ready = function () {
-            if (typeof this.start == 'function') this.start();
-            this.start = null;
-        };
-
-        this.addListener = function (target, name, callback, scope) {
-            function handler () { callback.call(scope || this); }
-
-            if (target.addEventListener) {
-                target.addEventListener(name, handler, false);
-            } else if (target.attachEvent) {
-                target.attachEvent('on' + name, handler);
-            } else {
-                target['on' + name] = handler;
-            }
-        };
-
-        this.removeListener = function (target, name, callback) {
-            if (target.removeEventListener) {
-                target.removeEventListener(name, callback, false);
-            } else {
-                target.detachEvent('on' + name, callback);
-            }
-        };
-
         this.setup = function (context) {
             context.bender = this;
             context.onerror = this.error;
-            this.addListener(context, 'load', this.ready, this);
         };
     }
 
