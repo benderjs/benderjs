118ceddee1823f9eeba5cb3a82b82bf563d05c08
Grzegorz Pabian <greg.pabian@gmail.com>
2014-06-24 12:03:48 +0200

    Added and fixed mocks for tests.

--- test/mocks.js
+++ test/mocks.js
@@ -2,46 +2,97 @@
  * @file Mockup to be used in tests
  */
 
-var moduleMocks = {
-	config: {}
-};
+'use strict';
+
+var moduleMocks;
 
 function nop() {}
 
-function App() {}
+moduleMocks = {
+	conf: function( bender ) {
+		bender.conf = {
+			applications: {
+				test: {
+					path: 'test/fixtures/apps/',
+					files: [
+						'test.js',
+						'test.css'
+					]
+				},
+
+				test2: {
+					proxy: 'http://localhost/',
+					files: [
+						'test.js',
+						'unknown'
+					]
+				}
+			},
+
+			browsers: [ 'Chrome', 'Firefox', 'Opera' ]
+		};
+	},
+
+	utils: function( bender ) {
+		bender.checkDeps = nop;
+
+		bender.utils = {};
+	}
+};
+
+function App() {
+	this._modules = {};
+	this._handlers = {};
+}
+
+App.prototype.emit = function( name, args ) {
+	if ( typeof this._handlers[ name ] == 'function' ) {
+		this._handlers[ name ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
+	}
+};
+
+App.prototype.on = function( name, callback ) {
+	this._handlers[ name ] = callback;
+};
 
 App.prototype.use = function( modules, options ) {
-	// single module added
-	if ( typeof modules === 'object' && modules !== null ) {
-		this[ module.name ] = module;
-
-		if ( typeof module.attach == 'function' ) {
-			module.attach.call( this, options );
-		}
-
-		return;
-	}
-
 	// array of modules added
 	if ( Array.isArray( modules ) ) {
 		modules.forEach( function( module ) {
 			this[ module.name ] = module;
+
+			this._modules[ module.name ] = module;
 
 			if ( typeof module.attach == 'function' ) {
 				module.attach.call( this );
 			}
 		}, this );
 	}
+
+	// single module added
+	if ( typeof modules === 'object' && modules !== null ) {
+		this[ modules.name ] = modules;
+
+		this._modules[ modules.name ] = modules;
+
+		if ( typeof modules.attach == 'function' ) {
+			modules.attach.call( this, options );
+		}
+
+		return;
+	}
 };
 
 App.prototype.init = function( callback ) {
 	// initialize modules
-	Object.keys( this ).forEach( function( name ) {
-		if ( typeof this[ name ] == 'object' && this[ name ] !== null &&
-			typeof this[ name ].init == 'function' && !this[ name ].initialized ) {
-			this[ name ].init( nop );
+	Object.keys( this._modules ).forEach( function( name ) {
+		var module = this._modules[ name ];
+
+		if ( typeof module == 'object' && module !== null &&
+			typeof module.init == 'function' && !module.initialized ) {
+			module.init.call( this, nop );
 		}
-	} );
+	}, this );
 
 	if ( typeof callback == 'function' ) {
 		callback();
@@ -52,8 +103,9 @@ function getBender( mocks ) {
 	var bender = new App();
 
 	if ( mocks ) {
+		mocks = Array.prototype.slice.call( arguments, 0 );
 		mocks.forEach( function( name ) {
-			bender[ name ] = moduleMocks[ name ];
+			moduleMocks[ name ]( bender );
 		} );
 	}
 
@@ -61,3 +113,9 @@ function getBender( mocks ) {
 }
 
 module.exports.getBender = getBender;
+
+module.exports.logger = {
+	error: nop,
+	info: nop,
+	debug: nop
+};
