4eaf774e98c5cf7b86c2e621e0d0cace94fd1b22
Grzegorz Pabian <greg.pabian@gmail.com>
2014-06-30 09:07:01 +0200

    Finished tests for html page builder.

--- test/pagebuilders/html.js
+++ test/pagebuilders/html.js
@@ -51,6 +51,25 @@ describe( 'Page Builders - Html', function() {
 		} );
 	} );
 
+	it( 'should load job\'s HTML from the job\'s directory', function() {
+		var data = {
+				jobId: 'foo',
+				html: 'fixtures/tests/test/1.html',
+				parts: []
+			},
+			expected = path.join( '.bender/jobs/', data.jobId, '/tests/', data.html );
+
+		function handle( err ) {
+			expect( err ).to.be.an( 'object' );
+			expect( err.code ).to.equal( 'ENOENT' );
+			expect( err.path ).to.equal( expected );
+		}
+
+		data = html.build( data );
+
+		return data.parts[ 0 ].done( handle, handle );
+	} );
+
 	it( 'should not alter data if no html specified', function() {
 		var data = {
 			parts: []
