ea87ccc64740b5b12a74e82806daacbf8b99c254
Grzegorz Pabian <greg.pabian@gmail.com>
2014-07-09 15:59:09 +0200

    Added: company headers to test files.

--- test/middleware/plugins.js
+++ test/middleware/plugins.js
@@ -1,4 +1,7 @@
 /**
+ * Copyright (c) 2014, CKSource - Frederico Knabben. All rights reserved.
+ * Licensed under the terms of the MIT License (see LICENSE.md).
+ *
  * @file Tests for Plugins middleware
  */
 
